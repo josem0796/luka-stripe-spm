ddae6e31152aa15454a114666d52d01493565ba5
Carlos Villamizar <cvillamizar@me.com>
2024-05-30 14:56:50 -0400

    Initial Commit

---
+++ LukaStripeApp/LukaStripeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LukaStripeAppApp.swift
+//  LukaStripeApp
+//
+//  Created by Jose Moran on 30/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LukaStripeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
